d8acfe49df415333baa3f6f00925be268e4f1c66
WalkerDogW <WalkerDogW@outlook.com>
2020-05-22 15:15:09 +0800

    Initial commit

---
+++ src/main/java/site/javaee/java_new_features/JavaNewFeaturesApplication.java
@@ -0,0 +1,13 @@
+package site.javaee.java_new_features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNewFeaturesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaNewFeaturesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/javaee/java_new_features/JavaNewFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package site.javaee.java_new_features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNewFeaturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
